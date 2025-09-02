f978d0694746d870350b6d8eabeddfd5782c8f82
Duc Minh Nguyen <2476537@edu.vaniercollege.qc.ca>
2025-09-02 12:24:43 -0400

    test code file added

---
+++ New folder - Copy/app.py
@@ -0,0 +1,14 @@
+# These are my calculations
+# x=5
+# y = x - 2
+# print(y)
+
+
+# name = input("Enter your name: ")
+
+# print("My name is " + name)
+
+age = int(input("Enter your age: "))
+
+age = age + 5
+print("my age is " + str(age))
